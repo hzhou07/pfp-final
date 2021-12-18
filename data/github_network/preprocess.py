ef6453982394f012f2f2e85e5d9a3613655a2f12
丘锐 <qiurui@macbook-pro.myfiosgateway.com>
2021-12-18 16:47:38 -0500

    process data

---
+++ data/github_network/preprocess.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+header = 1
+edges = defaultdict(list)
+with open ("musae_git_edges.csv") as f:
+    for l in f:
+        if header == 1:
+            header = 0
+            continue
+        s = l[:-1].split(',')[0]
+        t = l[:-1].split(',')[1]
+        edges[s].append(t)
+
+#print(edges)
+for e in edges.keys():
+    print(e + " "+ " ".join(edges[e]))
