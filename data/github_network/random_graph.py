8927076810f0e6a68dd043b246635c6fa62594f3
丘锐 <qiurui@macbook-pro.myfiosgateway.com>
2021-12-18 22:40:49 -0500

    simulation

---
+++ data/github_network/random_graph.py
@@ -0,0 +1,18 @@
+import networkx as nx
+from collections import defaultdict
+
+g = nx.generators.random_graphs.powerlaw_cluster_graph(100,5,0.2, seed = 0) 
+
+es = defaultdict(list)
+for e in g.edges:
+    s = e[0]
+    t = e[1]
+    if s == t:
+        continue
+    es[s].append(str(t))
+    es[t].append(str(s))
+
+#print(edges)
+for e in es.keys():
+    #print(e + str(edges[e]))
+    print(str(e) + " "+ " ".join(set(es[e])))
