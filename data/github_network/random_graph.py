2fd8d4a33e8c1c1b0dec310ca0c35455d077c6b9
丘锐 <qiurui@macbook-pro.myfiosgateway.com>
2021-12-20 14:32:33 -0500

    random script

--- data/github_network/random_graph.py
+++ data/github_network/random_graph.py
@@ -1,8 +1,9 @@
 import networkx as nx
 from collections import defaultdict
 
-g = nx.generators.random_graphs.powerlaw_cluster_graph(1000,5,0.2, seed = 0) 
-
+g = nx.generators.random_graphs.powerlaw_cluster_graph(20,10,0.2, seed = 0) 
+#g = nx.random_graphs.barabasi_albert_graph(,3)
+c=0
 es = defaultdict(list)
 for e in g.edges:
     s = e[0]
@@ -11,8 +12,10 @@ for e in g.edges:
         continue
     es[s].append(str(t))
     es[t].append(str(s))
+    c+=1
 
 #print(edges)
 for e in es.keys():
     #print(e + str(edges[e]))
     print(str(e) + " "+ " ".join(set(es[e])))
+print(c)
